d0d690de413bd2842f6d39e8374b0e8dd4b35458
dettone <dettonex25@gmail.com>
2021-08-17 00:50:03 -0300

    New Project

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // ----- Is a WEB Application
+import App from './App';
+import './services/firebase'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Put inside this
+); 
+
